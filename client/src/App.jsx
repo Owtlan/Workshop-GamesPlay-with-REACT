4500e36b76893b1fd1da8ef6e2b7ad7ead23b8f1
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-01-09 12:18:26 +0200

    Separate components from html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Login from "./components/login/Login"
 
 function App() {
 
     return (
-        <>
+        <div id="box">
             <h1>Games Play</h1>
-        </>
+            <Header />
+
+            <main id="main-content">
+                <Home />
+            </main>
+
+        </div>
     )
 }
 
