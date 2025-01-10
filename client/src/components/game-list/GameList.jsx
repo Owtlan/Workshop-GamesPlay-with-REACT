b8110d7fbd6fca325150e9f0c733050ea37591fb
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-01-10 11:29:31 +0200

    adding conditional rendering for no games

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -16,9 +16,10 @@ export default function GameList() {
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.map(game => <GameListItem key={game._id} {...game} />)}
-
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0
+                ? games.map(game => <GameListItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No Games yet</h3>
+            }
         </section>
     );
 }
