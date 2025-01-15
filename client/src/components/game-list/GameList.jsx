461956e427f585a22a9e6fab7fe0958b80bd505e
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-01-15 11:47:05 +0200

    adding useGames hook into my gameList

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from 'react';
-
-import * as gamesAPI from '../../api/games-api'
-
+import { useGetAllGames } from '../../hooks/useGames';
 import GameListItem from './game-list-item/GameListItem';
 
 
 export default function GameList() {
-    const [games, setGames] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            const result = await gamesAPI.getAll()
-
-            setGames(result)
-        })()
-    }, []);
-
+    const [games] = useGetAllGames();
 
     return (
         <section id="catalog-page">
