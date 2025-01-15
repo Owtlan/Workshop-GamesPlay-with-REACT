7ebd40d5dc0d309acb1b5d144b94bbfd2f5c4bac
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-01-15 11:46:03 +0200

    making my useGames hook

---
+++ client/src/hooks/useGames.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import gamesApi from '../api/games-api';
+
+export function useGetAllGames() {
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            const result = await gamesApi.getAll()
+
+            setGames(result);
+        })();
+    }, [])
+
+    return [games, setGames];
+
+}
