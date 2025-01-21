7bafeacaefad4f95dfed7f4072592b6d44fafaa1
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-01-21 10:04:54 +0200

    extract authData state managment from app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 
@@ -9,30 +8,13 @@ import Register from "./components/register/Register"
 import GameList from './components/game-list/GameList'
 import GameCreate from './components/game-create/GameCreate'
 import GameDetails from './components/game-details/GameDetails'
-import { AuthContext } from './contexts/AuthContext'
+import { AuthContextProvider } from './contexts/AuthContext'
 
 
 function App() {
-    const [authState, setAuthState] = useState({});
-
-    const changeAuthState = (state) => {
-
-        localStorage.setItem('accessToken', state.accessToken);
-
-        setAuthState(state);
-    }
-
-    const contextData = {
-        userId: authState._id,
-        email: authState.email,
-        accessToken: authState.accessToken,
-        isAuthenticated: !!authState.email,
-        changeAuthState,
-    }
-
     return (
 
-        <AuthContext.Provider value={contextData}>
+        <AuthContextProvider>
             <div id="box">
                 <Header />
 
@@ -48,7 +30,7 @@ function App() {
                 </main>
 
             </div>
-        </AuthContext.Provider>
+        </AuthContextProvider>
     )
 }
 
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,10 +1,9 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../contexts/AuthContext";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 
 export default function Header() {
-    const { isAuthenticated } = useContext(AuthContext)
+    const { isAuthenticated } = useAuthContext()
 
     return (
         <header>
