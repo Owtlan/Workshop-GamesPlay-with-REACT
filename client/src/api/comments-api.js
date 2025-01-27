3531d9cbff76c79d8f872af9757cf7ae2100ff80
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-01-27 22:03:41 +0200

    update comments api

--- client/src/api/comments-api.js
+++ client/src/api/comments-api.js
@@ -8,7 +8,8 @@ const create = (gameId, text) => requester.post(BASE_URL, { gameId, text })
 
 const getAll = (gameId) => {
     const params = new URLSearchParams({
-        where: `gameId="${gameId}"`
+        where: `gameId="${gameId}"`,
+        load: `author=_ownerId:users`
     })
 
 
